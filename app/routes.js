ca17e04b7870c5cb40c187a8c153667b14a8e3a0
Michael Cheung <michaelcheung@nhs.net>
2024-02-13 16:20:53 +0000

    Add error message errors

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,26 @@ router.get('/service/sitemap.xml', function(req, res) {
     res.sendFile('sitemap.xml', { root: './app/views/service' });
 });
 
+router.post('/service/step1', function(req, res) {
+
+    let firstName = req.session.data['first-name']
+    let lastName = req.session.data['last-name']
+
+    if (firstName == ""){
+      if (lastName == ""){
+        res.render('service/step1', {error: 1, noFirstName: 1, noLastName: 1})
+      } else {
+        res.render('service/step1', {error: 1, noFirstName: 1})
+      }
+    } else {
+      if (lastName == ""){
+        res.render('service/step1', {error: 1, noLastName: 1})
+      } else {
+        res.redirect('/service/step2')
+      }
+    }
+
+});
+
+
 module.exports = router;
